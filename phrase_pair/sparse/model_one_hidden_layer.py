b1f37a908803997463335b5eedc1e506878490e4
jxnw <58523003+jxnw@users.noreply.github.com>
2022-01-24 14:53:37 +0000

    Implement RNN for translation confidence

--- phrase_pair/sparse/model_one_hidden_layer.py
+++ phrase_pair/sparse/model_one_hidden_layer.py
@@ -9,6 +9,7 @@ path_to_phrase_pair_id = "../phrase_pair_id"
 path_to_sentence_avg_score = "../sentence_avg_score"
 
 
+# ============================================ data pre-processing
 class PhrasePairDataset(Dataset):
     def __init__(self, sentences, scores):
         self.sentences = sentences
@@ -23,6 +24,39 @@ class PhrasePairDataset(Dataset):
         return sentence, score
 
 
+def count_line(file):
+    with open(file, "r") as f:
+        return len(f.readlines())
+
+
+def preprocess_data(phrase_pair_id_file):
+    # get one hot encodings from sentence pairs
+    phrase_pair_encodings = np.zeros((training_size, 200001), dtype=bool)
+    with open(phrase_pair_id_file, "r") as ppt:
+        for count, line in enumerate(ppt):
+            phrase_ids = line.split()
+            for p_id in phrase_ids:
+                phrase_pair_encodings[count, int(p_id)] = True
+    return torch.BoolTensor(phrase_pair_encodings)
+
+
+def load_expected(expected_file):
+    expected = np.loadtxt(expected_file)
+    expected = np.atleast_2d(expected).T
+    return torch.Tensor(expected)
+
+
+# initialise the PhrasePairDataset class
+training_size = count_line(path_to_sentence_avg_score)
+
+tensor_sentences = preprocess_data(path_to_phrase_pair_id)
+tensor_scores = load_expected(path_to_sentence_avg_score)
+
+phrase_pair_dataset = PhrasePairDataset(tensor_sentences, tensor_scores)
+phrase_pair_loader = DataLoader(phrase_pair_dataset)
+
+
+# ============================================ model training
 class OneHiddenLayerNet(torch.nn.Module):
     def __init__(self, input_size, hidden_size):
         super(OneHiddenLayerNet, self).__init__()
@@ -40,39 +74,18 @@ class OneHiddenLayerNet(torch.nn.Module):
         return output
 
 
-training_size = 28327
-
-# get one hot encodings from sentence pairs
-phrase_pair_encodings = np.zeros((training_size, 200001), dtype=bool)
-with open(path_to_phrase_pair_id, "r") as ppt:
-    for count, line in enumerate(ppt):
-        phrase_ids = line.split()
-        for p_id in phrase_ids:
-            phrase_pair_encodings[count, int(p_id)] = True
-
-# get average feature score
-expected_score = np.loadtxt(path_to_sentence_avg_score)
-expected_score = np.atleast_2d(expected_score).T
-
-# Initialise the PhrasePairDataset class
-tensor_sentences = torch.BoolTensor(phrase_pair_encodings)
-tensor_scores = torch.Tensor(expected_score)
-
-phrase_pair_dataset = PhrasePairDataset(tensor_sentences, tensor_scores)
-phrase_pair_loader = DataLoader(phrase_pair_dataset)
-
 # Hyper-parameters
 learning_rate = 1e-3
 batch_size = 100
 epoch = 10
 
-# Create a model
+# create a model
 model = OneHiddenLayerNet(200001, 20)
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-for param in model.state_dict():
-    print(param)
+# for param in model.state_dict():
+#     print(param)
 
 
 def train_loop(dataloader, model, loss_fn, optimizer):
@@ -100,9 +113,9 @@ model.train()
 for epoch_id in range(epoch):
     print(f"Epoch {epoch_id + 1}\n-------------------------------")
     confidence_score = train_loop(phrase_pair_loader, model, loss_fn, optimizer)
-    torch.save({
-        'epoch': epoch,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict()
-    }, "one_hidden_layer/model_epoch{}.pth".format(epoch_id + 1))
-    np.savetxt("one_hidden_layer/score_epoch{}".format(epoch_id + 1), confidence_score)
+    # torch.save({
+    #     'epoch': epoch,
+    #     'model_state_dict': model.state_dict(),
+    #     'optimizer_state_dict': optimizer.state_dict()
+    # }, "one_hidden_layer/model_epoch{}.pth".format(epoch_id + 1))
+    # np.savetxt("one_hidden_layer/score_epoch{}".format(epoch_id + 1), confidence_score)
